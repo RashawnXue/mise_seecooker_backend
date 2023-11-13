57e5b4a077b69f2e7bed4f692765ed2d29ffbdd0
Rashawn <ruichenxue@smail.nju.edu.cn>
2023-11-14 01:20:58 +0800

    init repo

---
+++ src/main/java/com/mise/mise_seecooker_backend/MiseSeecookerBackendApplication.java
@@ -0,0 +1,13 @@
+package com.mise.mise_seecooker_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiseSeecookerBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MiseSeecookerBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mise/mise_seecooker_backend/MiseSeecookerBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mise.mise_seecooker_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiseSeecookerBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
